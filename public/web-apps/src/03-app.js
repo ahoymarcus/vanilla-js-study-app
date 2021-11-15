926f78b554c7619993b0ff921bc01d6f1e32afde
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-15 19:08:06 -0300

    Iniciar primeiros exercicios. Exercicios 1, 2 e 3

---
+++ public/web-apps/src/03-app.js
@@ -0,0 +1,15 @@
+// document.write('hello world');
+// alert('hello world');
+// console.log('hello world');
+
+/* 
+    SEE: the console.log() function is the
+    more powerful
+*/
+document.write({ name: 'john' });
+alert({ name: 'john' });
+console.log({ name: 'john' })
+
+
+
+
