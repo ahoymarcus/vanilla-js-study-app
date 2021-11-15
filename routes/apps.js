9ae78def4ecf20607ad68c19f306d4a9b8dd3dd4
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-15 15:11:21 -0300

    Corrigir estrutura de pastas do servidor

--- routes/apps.js
+++ routes/apps.js
@@ -6,7 +6,7 @@ const path = require('path');
 const basicApp = (req, res) => {
 	
 	// res.status(200).send('Basic App route');
-	res.sendFile(path.resolve(__dirname, '../public/web-app/assets/index.html'));
+	res.sendFile(path.resolve(__dirname, '../public/web-apps/assets/index.html'));
 };
 
 
